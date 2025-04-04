cab5b0f3c3c22c0438f31344dfdf837fb795bf1f
Jan De Kock <jan@twofold.be>
2025-04-04 20:54:35 +0200

    Move to ImageSharp for testing

--- TinyBCSharpTests/BCTestUtils.cs
+++ TinyBCSharpTests/BCTestUtils.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Runtime.InteropServices;
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 
 namespace TinyBCSharpTests;
 
@@ -12,24 +9,12 @@ public static class BCTestUtils
 
     public static byte[] ReadPng(string path)
     {
-        var bitmap = new Bitmap(path);
-        var bitmapData = bitmap.LockBits(
-            new Rectangle(0, 0, bitmap.Width, bitmap.Height),
-            ImageLockMode.ReadOnly,
-            PixelFormat.Format32bppArgb
-        );
-
-        var numPixels = bitmapData.Width * bitmapData.Height;
-        var rawImage = new byte[numPixels * 4];
-        Marshal.Copy(bitmapData.Scan0, rawImage, 0, rawImage.Length);
-        bitmap.UnlockBits(bitmapData);
-
-        for (var i = 0; i < rawImage.Length; i += 4)
-        {
-            (rawImage[i], rawImage[i + 2]) = (rawImage[i + 2], rawImage[i]);
-        }
-
-        return rawImage;
+        using var image = Image.Load(path);
+        var pixelArray = new byte[image.Width * image.Height * 4];
+        image
+            .CloneAs<Rgba32>()
+            .CopyPixelDataTo(pixelArray);
+        return pixelArray;
     }
 
     public static byte[] ReadDdsFp16(string path, int width, int height)
